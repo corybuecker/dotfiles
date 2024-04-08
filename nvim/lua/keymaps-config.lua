d6acdea4d4d9b28b1d77f7fdea9303a377edab36
Cory Buecker <email@corybuecker.com>
2024-04-08 07:56:49 -0500

    add simple command to run Swift REPL

--- nvim/lua/keymaps-config.lua
+++ nvim/lua/keymaps-config.lua
@@ -1,5 +1,6 @@
 vim.keymap.set('n', '<leader>fb', ':Telescope file_browser path=%:p:h <CR>')
 vim.keymap.set('n', '<leader>tf', ':TestFile<CR>')
+vim.keymap.set('n', '<leader>rs', ':lua vim.cmd.RunSwift()<CR>')
 
 vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(args)
@@ -7,3 +8,10 @@ vim.api.nvim_create_autocmd('LspAttach', {
   end,
 })
 
+vim.api.nvim_create_user_command('RunSwift',
+  function()
+    vim.cmd('w')
+    vim.cmd('!swift %')
+  end,
+  {}
+)
