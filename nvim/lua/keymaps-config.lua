d7bdac13ad8f3774f4d2283135e624bdc8c6d8a1
Cory Buecker <email@corybuecker.com>
2024-04-14 10:43:45 -0500

    add buffer change keymap

--- nvim/lua/keymaps-config.lua
+++ nvim/lua/keymaps-config.lua
@@ -1,6 +1,7 @@
 vim.keymap.set('n', '<leader>fb', ':Telescope file_browser path=%:p:h <CR>')
 vim.keymap.set('n', '<leader>tf', ':TestFile<CR>')
 vim.keymap.set('n', '<leader>rs', ':lua vim.cmd.RunSwift()<CR>')
+vim.keymap.set('n', '<leader>b', ':buffers<CR>:buffer<Space>')
 
 vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(args)
