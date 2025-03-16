ee90f23ba284140a199cc38c980e9b86ce14e586
Cory Buecker <email@corybuecker.com>
2025-03-16 06:05:05 -0500

    chore: add jump-to-error

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,3 @@
 vim.keymap.set("n", "<leader>fb", require("fzf-lua").files)
 vim.keymap.set("n", "<leader>b", require("fzf-lua").buffers)
-vim.keymap.set("n", "<leader>c", vim.cmd.PrtPopup)
+vim.keymap.set("n", "gn", vim.diagnostic.goto_next)
